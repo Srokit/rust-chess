a7d3d3b1f23faac23328c3b32f0fbb41b15d9513
Stan <stan@stansa.dev>
2023-06-08 16:17:54 -0700

    Rook moves added

--- src/check_move.rs
+++ src/check_move.rs
@@ -13,7 +13,7 @@ const WHITE_PAWN_STARTING_ROW: u8 = 6;
 pub struct CheckMoveInfo {
     pub from: position::Position,
     pub to: position::Position,
-    pub from_piece: piece::Piece,
+    pub from_piece: Option<piece::Piece>,
     pub to_piece: Option<piece::Piece>,
     pub is_capturing_piece: bool,
 }
@@ -23,7 +23,7 @@ impl CheckMoveInfo {
         CheckMoveInfo {
             from: *from,
             to: *to,
-            from_piece: board.copy_piece_at_position(from).unwrap(),
+            from_piece: board.copy_piece_at_position(from),
             to_piece: board.copy_piece_at_position(to),
             is_capturing_piece: board.copy_piece_at_position(to).is_some(),
         }
@@ -38,36 +38,38 @@ pub fn check_move(board: &board::Board, move_info: &CheckMoveInfo) -> bool {
     match from_piece_type {
         piece::PieceType::Pawn => return _pawn_move(board, move_info),
         piece::PieceType::Rook => return _rook_move(board, move_info),
+        piece::PieceType::Bishop => return _bishop_move(board, move_info),
         // TODO(Stan): Account for other types
         _ => return true,
     }
 }
 
 fn _any_type(move_info: &CheckMoveInfo) -> bool {
-    let from = move_info.from;
-    let to = move_info.to;
-    let from_piece = move_info.from_piece;
+    if move_info.from_piece.is_none() {
+        return false;
+    }
+
+    let from_piece = move_info.from_piece.unwrap();
     let to_piece = move_info.to_piece;
-    let is_capturing_piece = to_piece.is_some();
-    let from_color = from_piece.get_color();
 
     // Player always plays White for now
-    if from_color == piece::Color::Black {
+    if from_piece.get_color() == piece::Color::Black {
         return false;
     }
     // King cannot be captured
-    if is_capturing_piece && to_piece.unwrap().get_piece_type() == piece::PieceType::King {
+    if move_info.is_capturing_piece && to_piece.unwrap().get_piece_type() == piece::PieceType::King {
         return false;
     }
     // Cannot capture own pieces
-    if is_capturing_piece && to_piece.unwrap().get_color() == from_color {
+    if move_info.is_capturing_piece && to_piece.unwrap().get_color() == from_piece.get_color() {
         return false;
     }
     // Cannot stay put
-    if from == to {
+    if move_info.from == move_info.to {
         return false;
     }
 
+    // Rest will be checked in piece specific move checkers
     true
 }
 
@@ -124,6 +126,41 @@ fn _rook_move(board: &board::Board, move_info: &CheckMoveInfo) -> bool {
         } else {
             row += direction_r;
         }
+        let curr_pos = position::Position::new(row as u8, col as u8);
+        if curr_pos == to {
+            break;
+        }
+        let curr_piece = board.copy_piece_at_position(&curr_pos);
+        if curr_piece.is_some() {
+            return false;
+        }
+    }
+
+    // Other cases must be valid
+    true
+}
+
+fn _bishop_move(board: &board::Board, move_info: &CheckMoveInfo) -> bool {
+    let from = move_info.from;
+    let to = move_info.to;
+    let diff_c = _diff_u8_as_i8(from.get_col(), to.get_col());
+    let diff_r = _diff_u8_as_i8(from.get_row(), to.get_row());
+
+    // Can only move diagonally
+    if diff_c.abs() != diff_r.abs() {
+        return false;
+    }
+
+    let direction_c = if diff_c > 0 {1} else {-1};
+    let direction_r= if diff_r > 0 {1} else {-1};
+
+    // Is there something in the way
+    let mut row = from.get_row() as i8;
+    let mut col = from.get_col() as i8;
+    loop {
+        // Move to next position
+        col += direction_c;
+        row += direction_r;
         let curr_pos = position::Position::new(row as u8, col as u8);
         if curr_pos == to {
             break;
